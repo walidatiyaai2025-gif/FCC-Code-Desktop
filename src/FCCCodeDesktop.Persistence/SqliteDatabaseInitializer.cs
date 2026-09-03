8148b3459450c3ee643a16adb9c6a13875ce022b
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-03 21:46:34 +0300

    P03-001: satisfy concrete migration-plan analyzer

--- src/FCCCodeDesktop.Persistence/SqliteDatabaseInitializer.cs
+++ src/FCCCodeDesktop.Persistence/SqliteDatabaseInitializer.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Globalization;
 using Microsoft.Data.Sqlite;
 
@@ -49,7 +50,7 @@ public sealed class SqliteDatabaseInitializer
             Array.AsReadOnly(newlyAppliedVersions.ToArray()));
     }
 
-    private static IReadOnlyList<SqliteMigration> BuildMigrationPlan(
+    private static ReadOnlyCollection<SqliteMigration> BuildMigrationPlan(
         IEnumerable<SqliteMigration>? additionalMigrations)
     {
         var migrations = new List<SqliteMigration>(SqliteSchema.BaselineMigrations);
