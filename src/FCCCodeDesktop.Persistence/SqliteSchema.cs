48c5f3e997563fa779326feca8565fe048c78cb7
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-03 21:49:11 +0300

    P03-001: use concrete baseline migration collection

--- src/FCCCodeDesktop.Persistence/SqliteSchema.cs
+++ src/FCCCodeDesktop.Persistence/SqliteSchema.cs
@@ -1,10 +1,12 @@
+using System.Collections.ObjectModel;
+
 namespace FCCCodeDesktop.Persistence;
 
 internal static class SqliteSchema
 {
     public const int BaselineVersion = 1;
 
-    public static IReadOnlyList<SqliteMigration> BaselineMigrations { get; } =
+    public static ReadOnlyCollection<SqliteMigration> BaselineMigrations { get; } =
         Array.AsReadOnly(
         [
             new SqliteMigration(
