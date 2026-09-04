56edd3b5a2faced7b9df5e3fa261144db1fde1f9
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-04 04:07:28 +0300

    P04-001: add environment discovery options

---
+++ src/FCCCodeDesktop.Fcc/FccEnvironmentDiscoveryOptions.cs
@@ -0,0 +1,20 @@
+namespace FCCCodeDesktop.Fcc;
+
+public sealed class FccEnvironmentDiscoveryOptions
+{
+    public string? FccClaudeExecutablePath { get; init; }
+
+    public string? FccServerExecutablePath { get; init; }
+
+    public string? PathValue { get; init; }
+
+    public string? PathExtensions { get; init; }
+
+    public int? FccServerPort { get; init; }
+
+    public Uri? HealthUri { get; init; }
+
+    public TimeSpan ProcessTimeout { get; init; } = TimeSpan.FromSeconds(5);
+
+    public TimeSpan HealthTimeout { get; init; } = TimeSpan.FromSeconds(3);
+}
