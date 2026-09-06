09b3ffa8652db3e90154e53bd3a6b40154f8e630
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-06 08:04:28 +0300

    feat: wire lazy explorer to project surface state

--- src/FCCCodeDesktop.App/Projects/ProjectWorkspaceSurface.xaml.cs
+++ src/FCCCodeDesktop.App/Projects/ProjectWorkspaceSurface.xaml.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using FCCCodeDesktop.Core.State;
+using FCCCodeDesktop.Files;
 using Microsoft.Win32;
 
 namespace FCCCodeDesktop.App.Projects;
@@ -12,10 +14,11 @@ public partial class ProjectWorkspaceSurface : UserControl
         nameof(State),
         typeof(ProjectWorkspaceState),
         typeof(ProjectWorkspaceSurface),
-        new PropertyMetadata(null));
+        new PropertyMetadata(null, OnStateChanged));
 
     public ProjectWorkspaceSurface()
     {
+        FileExplorerState = new ProjectFileExplorerState(new FileSystemProjectFileExplorerService());
         InitializeComponent();
     }
 
@@ -23,6 +26,35 @@ public partial class ProjectWorkspaceSurface : UserControl
     {
         get => (ProjectWorkspaceState?)GetValue(StateProperty);
         set => SetValue(StateProperty, value);
+    }
+
+    public ProjectFileExplorerState FileExplorerState { get; }
+
+    private static void OnStateChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+    {
+        var surface = (ProjectWorkspaceSurface)dependencyObject;
+        if (e.OldValue is ProjectWorkspaceState oldState)
+        {
+            oldState.PropertyChanged -= surface.OnProjectStatePropertyChanged;
+        }
+
+        if (e.NewValue is ProjectWorkspaceState newState)
+        {
+            newState.PropertyChanged += surface.OnProjectStatePropertyChanged;
+            surface.FileExplorerState.SetProject(newState.ActiveProject);
+        }
+        else
+        {
+            surface.FileExplorerState.SetProject(null);
+        }
+    }
+
+    private void OnProjectStatePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(ProjectWorkspaceState.ActiveProject) or null)
+        {
+            FileExplorerState.SetProject(State?.ActiveProject);
+        }
     }
 
     private async void OnOpenProjectClick(object sender, RoutedEventArgs e)
@@ -83,6 +115,33 @@ public partial class ProjectWorkspaceSurface : UserControl
         }
 
         await ExecuteProjectActionAsync(() => state.RefreshTechnologyDetectionAsync(CancellationToken.None));
+    }
+
+    private void OnRefreshFileExplorerClick(object sender, RoutedEventArgs e) =>
+        FileExplorerState.Refresh();
+
+    private async void OnFileExplorerNodeExpanded(object sender, RoutedEventArgs e)
+    {
+        if (sender is not TreeViewItem { DataContext: ProjectFileTreeNode node } item
+            || !ReferenceEquals(e.OriginalSource, item)
+            || !node.CanExpand
+            || node.ChildrenLoaded
+            || node.IsLoading)
+        {
+            return;
+        }
+
+        try
+        {
+            await FileExplorerState.LoadChildrenAsync(node, CancellationToken.None).ConfigureAwait(true);
+        }
+        catch (Exception exception) when (exception is IOException
+                                           or UnauthorizedAccessException
+                                           or ArgumentException
+                                           or InvalidOperationException)
+        {
+            // ProjectFileExplorerState records the actionable message inline on the affected node.
+        }
     }
 
     private static async Task ExecuteProjectActionAsync(Func<Task> action)
